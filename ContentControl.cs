496846a9c28afb9ced5a4da80a98d92c0a85c18a
Govert van Drimmelen <govert@dnakode.com>
2023-10-20 09:52:36 +0200

    Add project files.

---
+++ ContentControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace TestCustomTaskPane
+{
+    public interface IMyUserControl { }
+    
+    [ComDefaultInterface(typeof(IMyUserControl))]
+    public partial class ContentControl : UserControl, IMyUserControl
+    {
+        public ContentControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ribbon.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using ExcelDna.Integration.CustomUI;
+
+namespace TestCustomTaskPane
+{
+    [ComVisible(true)]
+    public class MyRibbon : ExcelRibbon
+    {
+        public override string GetCustomUI(string RibbonID)
+        {
+            Debug.Print("CustomTaskPane.GetCustomUI");
+            return
+@"<customUI xmlns='http://schemas.microsoft.com/office/2006/01/customui' loadImage='LoadImage'>
+    <ribbon>
+    <tabs>
+        <tab id='CustomTab' label='Custom Task Pane Test'>
+        <group id='SampleGroup' label='CTP Control'>
+            <button id='Button1' label='Show CTP' size='large' onAction='OnShowCTP' />
+            <button id='Button2' label='Delete CTP' size='large' onAction='OnDeleteCTP' />
+        </group >
+        </tab>
+    </tabs>
+    </ribbon>
+</customUI>
+";
+        }
+
+        public void OnShowCTP(IRibbonControl control)
+        {
+            CTPManager.ShowCTP();
+        }
+
+
+        public void OnDeleteCTP(IRibbonControl control)
+        {
+            CTPManager.DeleteCTP();
+        }
+    }
+}
